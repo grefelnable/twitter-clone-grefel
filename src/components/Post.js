6c37ff407710c1fb3dc8fae152f3cbb091f65a29
Grefel <felnable@gmail.com>
2022-05-05 14:27:30 -0400

    added Post js. done with tweetbox UI

---
+++ src/components/Post.js
@@ -0,0 +1,12 @@
+import React from "react"
+import tw from "tailwind-styled-components"
+
+const Post = ({ displayName, username, verified, text, image, avatar }) => {
+  return <PostContainer></PostContainer>
+}
+
+export default Post
+
+const PostContainer = tw.article`
+
+`
